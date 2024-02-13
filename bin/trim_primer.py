591364b26fdd54dc0521c292e854f572a826fe05
mcmero <cmerom@gmail.com>
2024-02-14 09:52:05 +1100

    Add percentages to trimming stats

--- bin/trim_primer.py
+++ bin/trim_primer.py
@@ -9,8 +9,8 @@ Portability : POSIX
 '''
 import gzip
 import edlib
+import os
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
-from datetime import datetime
 from argparse import ArgumentParser
 
 tab = str.maketrans("ACTG", "TGAC")
@@ -110,10 +110,15 @@ def main():
                 untrimmed_out.write("@%s\n%s\n+\n%s\n" % (read_id, read_seq, read_qual))
 
     # print stats to file
-    with open("trim_primer_stats.txt", "w") as stats_file:
-        stats_file.write("Trimmed reads: %d\n" % trimmed_reads)
+    sample_name = os.path.basename(args.reads).split(".")[0]
+    stats_file_name = f"{sample_name}_trimming_stats.txt"
+    percent_trimmed = round((trimmed_reads/total_reads)*100, 2)
+    percent_untrimmed = round((untrimmed_reads/total_reads)*100, 2) if untrimmed_out else 0
+
+    with open(stats_file_name, "w") as stats_file:
+        stats_file.write(f"Trimmed reads: {trimmed_reads} ({percent_trimmed}%)\n")
         if untrimmed_out:
-            stats_file.write("Untrimmed reads: %d\n" % untrimmed_reads)
+            stats_file.write(f"Untrimmed reads: {untrimmed_reads} ({percent_untrimmed}%)\n")
         stats_file.write("Total reads: %d\n" % total_reads)
         stats_file.write("Mismatches: %d\n" % args.mismatches)
         stats_file.write("Barcode length: %d" % args.barcode_length)
